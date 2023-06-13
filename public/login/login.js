5d3e19a9e4a89bb2b49f0f586627ecc94ac45113
Sofía Alonso <sofiaa@MacBook-Air-de-Sofia-2.local>
2023-06-13 11:29:17 -0300

    cambios

--- public/login/login.js
+++ public/login/login.js
@@ -1,26 +1,3 @@
-
-//Funcion para que se mueva el form de login al de registro
-/*const formsignup = document.getElementById("signup");
-formsignup.addEventListener('submit', signup);
-
-var x = document.getElementById("login")
-var y = document.getElementById("signup")
-var z = document.getElementById("elegir")
-
-function login_des(){
-    x.style.left = "50px"
-    y.style.left = "450px"
-    z.style.left = "0px"
-}
-function signup_des(){
-    x.style.left = "-400px"
-    y.style.left = "50px"
-    z.style.left = "130px"
-}
-
-*/
-
-
 //Se crea un js a parte para el login para tener mejor organización y más seguridad
 class Login {
     constructor() {  
